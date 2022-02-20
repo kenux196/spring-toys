edc2d1a7820ba321b46ce21793fca60e42c0facb
skyun <skyun@a2tec.co.kr>
2022-02-20 21:09:07 +0900

    request mapping 다양한 예시

---
+++ springmvc/src/main/java/me/kenux/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,67 @@
+package me.kenux.springmvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class MappingController {
+
+    @RequestMapping("hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "ok";
+    }
+
+    @RequestMapping(value = "/mapping-get-v1", method = RequestMethod.GET)
+    public String mappingGetV1() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    @GetMapping("/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mappingGetV1");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userid = {}", data);
+        return data;
+    }
+
+    /**
+     * PathVariable - 다중
+     */
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    /**
+     * 특정 파라미터 조건 매핑
+     */
+    @GetMapping(value = "/mapping-param", params = "mode=debug")
+    public String mappingParam() {
+        log.info("mappingParam");
+        return "ok";
+    }
+
+    /**
+     * 미디어 타입 조건 매핑
+     */
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsume() {
+        log.info("mappingConsume");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
