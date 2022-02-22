5be818f36cc4ff7ed0f4c4ed9424fbfcd5e5507e
skyun <skyun@a2tec.co.kr>
2022-02-22 18:21:01 +0900

    Spring security 사용자 확인 페이지 추가

--- spring-security/src/main/java/me/kenux/springsecurity/controller/MemberController.java
+++ spring-security/src/main/java/me/kenux/springsecurity/controller/MemberController.java
@@ -4,6 +4,7 @@ import me.kenux.springsecurity.domain.Member;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,6 +21,12 @@ public class MemberController {
         userA.setId(1L);
         final List<Member> members = List.of(userA);
         return ResponseEntity.ok(members);
+    }
+
+    @GetMapping("/whoami")
+    public String me(Authentication authentication, Model model) {
+        model.addAttribute("username", authentication.getName());
+        return "me";
     }
 
 }
