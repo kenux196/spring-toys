0a60a90a2e6046ecb491bc7eb6e97600b1146777
skyun <skyun@a2tec.co.kr>
2022-03-13 15:08:20 +0900

    Spring MVC 2 : Bean Validation 1

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -8,19 +8,24 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+// 아래는 기능이 약하고 번거로워서 권장되지 않는다. 차라리 자바코드로 로직을 처리하기를 권장한다.
+//@ScriptAssert(lang = "javascript",
+//        script = "_this.price * _this.quantity >= 10000",
+//        message = "총합이 10000원 넘게 입력해 주세요")
 public class Item {
 
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank
+    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1_000_000)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1_000_000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
