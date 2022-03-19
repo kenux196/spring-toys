8dc3e34592906dd860e28f5d628f4a317b84a42c
skyun <skyun@a2tec.co.kr>
2022-03-19 20:57:52 +0900

    Spring MVC : Type Converter  init

---
+++ type-converter/src/test/java/me/kenux/typeconverter/TypeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kenux.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
