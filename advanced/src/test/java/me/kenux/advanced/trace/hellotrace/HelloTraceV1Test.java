9fff34004221792a1829ea0c7212fafffa9d3681
skyun <skyun@a2tec.co.kr>
2022-03-25 00:08:14 +0900

    Add Log trace v1

---
+++ advanced/src/test/java/me/kenux/advanced/trace/hellotrace/HelloTraceV1Test.java
@@ -0,0 +1,21 @@
+package me.kenux.advanced.trace.hellotrace;
+
+import me.kenux.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.Test;
+
+class HelloTraceV1Test {
+
+    @Test
+    void begin_end() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.end(status);
+    }
+
+    @Test
+    void begin_ex() {
+        HelloTraceV1 trace = new HelloTraceV1();
+        TraceStatus status = trace.begin("hello");
+        trace.exception(status, new IllegalStateException());
+    }
+}
