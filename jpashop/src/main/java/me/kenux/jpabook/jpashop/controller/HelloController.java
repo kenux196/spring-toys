4391a725ececd9ee4b16cff5a2a4cecc6f7fc534
skyun <skyun@a2tec.co.kr>
2022-03-26 01:34:21 +0900

    [JPA] Add view : thymeleaf

--- jpashop/src/main/java/me/kenux/jpabook/jpashop/controller/HelloController.java
+++ jpashop/src/main/java/me/kenux/jpabook/jpashop/controller/HelloController.java
@@ -1,9 +1,11 @@
 package me.kenux.jpabook.jpashop.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class HelloController {
 
@@ -11,5 +13,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
     }
 }
--- jpashop/src/main/java/me/kenux/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/me/kenux/jpabook/jpashop/repository/OrderRepository.java
@@ -1,10 +1,16 @@
 package me.kenux.jpabook.jpashop.repository;
 
 import lombok.RequiredArgsConstructor;
+import me.kenux.jpabook.jpashop.domain.Member;
 import me.kenux.jpabook.jpashop.domain.Order;
+import me.kenux.jpabook.jpashop.domain.OrderSearch;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -24,4 +30,64 @@ public class OrderRepository {
 //    public List<Order> findAll(OrderSearch orderSearch) {
 //
 //    }
+
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql = "select o from Order o join o.member m";
+        boolean isFirstCondition = true;
+
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+
+        return query.getResultList();
+    }
+
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Order, Member> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name = cb.like(m.<String>get("name"), "%" + orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000);
+        return query.getResultList();
+    }
 }
