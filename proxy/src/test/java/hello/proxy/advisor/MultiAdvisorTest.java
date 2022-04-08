8d6d7579d3789843e905a9e60bd9fc0ed31a07d0
skyun <skyun@a2tec.co.kr>
2022-04-09 00:56:54 +0900

    스프링 프록시 - 멀티 프록시, 멀티 어드바이저 예제

---
+++ proxy/src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,73 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.ServiceImpl;
+import hello.proxy.common.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+@Slf4j
+class MultiAdvisorTest {
+
+    @Test
+    @DisplayName("여러 프록시")
+    void multiAdvisorTest1() {
+        // client -> proxy2(advisor2) -> proxy1(advisor1) -> target
+
+        // 프록시1 생성
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
+
+        // 프록시2 생성, target -> proxy1  입력
+        ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        proxyFactory2.addAdvisor(advisor2);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
+
+        proxy2.save();
+    }
+
+    @Test
+    @DisplayName("하나의 프록시, 여러 어브바이저 - 등록하는 순서대로  advisor가 호출된다.")
+    void multiAdvisorTest2() {
+
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+
+        // proxy -> advisor2 -> advisor1 -> target
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvisor(advisor2);
+        proxyFactory.addAdvisor(advisor1);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        // 스프링 AOP는 target 하나 당 하나의 프록시만 생성한다.
+        // 하나의 프록시에 여러 개의 어드바이저를 등록할 수 있다.
+    }
+
+
+    static class Advice1 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+
+    static class Advice2 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+}
