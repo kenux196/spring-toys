94d1b6779aa58eb59ddeac117f608270580416c6
skyun <skyun@a2tec.co.kr>
2022-04-11 11:45:43 +0900

    Add project - designpattern

---
+++ designpattern/src/test/java/me/kenux/designpattern/DesignpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kenux.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
