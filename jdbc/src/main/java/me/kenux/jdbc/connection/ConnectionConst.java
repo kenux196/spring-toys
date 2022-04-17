608f8332eaf735b160047d935f2a64a019b3e84d
skyun <skyun@a2tec.co.kr>
2022-04-17 21:24:22 +0900

    [Spring DB] JDBC - Connection

---
+++ jdbc/src/main/java/me/kenux/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package me.kenux.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:h2:tcp://localhost/~/h2dbs/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
---
+++ jdbc/src/main/java/me/kenux/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,27 @@
+package me.kenux.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static me.kenux.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class DBConnectionUtil {
+
+    private DBConnectionUtil() {
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info("get connection={}, class={}", connection, connection.getClass());
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException();
+        }
+    }
+}
