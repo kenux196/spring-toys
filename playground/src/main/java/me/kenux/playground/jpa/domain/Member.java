d396fffa9602daf597ba263fb7ed0449f719de36
skyun <skyun@a2tec.co.kr>
2022-06-19 23:24:52 +0900

    [playground] mapping

--- playground/src/main/java/me/kenux/playground/jpa/domain/Member.java
+++ playground/src/main/java/me/kenux/playground/jpa/domain/Member.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "member")
@@ -15,8 +16,22 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false, length = 10)
     private String name;
     private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     @ManyToOne
     @JoinColumn(name = "team_id")
