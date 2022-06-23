65307fb86e7d9d7eaeb3c6241fd04f643dcfc8f7
skyun <skyun@a2tec.co.kr>
2022-06-23 22:18:17 +0900

    update, remove test

--- playground/src/test/java/me/kenux/playground/jpa/mapping/ManyToOneTest.java
+++ playground/src/test/java/me/kenux/playground/jpa/mapping/ManyToOneTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import me.kenux.playground.config.QuerydslConfig;
 import me.kenux.playground.jpa.domain.Member;
 import me.kenux.playground.jpa.domain.Team;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -11,6 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,21 +27,89 @@ class ManyToOneTest {
     EntityManager em;
 
     @Test
-    @Transactional
     void save() {
         Team team = new Team("teamA");
         em.persist(team);
 
         Member member1 = new Member("member1", 20);
-        em.persist(member1);
         member1.assignTeam(team);
+        em.persist(member1);
 
         em.flush();
-        em.close();
+        em.clear();
 
         Member findMember = em.find(Member.class, member1.getId());
         assertThat(findMember).isNotNull();
         assertThat(findMember.getTeam().getName()).isEqualTo("teamA");
         log.info("findMember = {}", findMember);
+    }
+
+    @Test
+    void update() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member member1 = new Member("member1", 20);
+        member1.assignTeam(teamA);
+        em.persist(member1);
+
+        Team teamB = new Team("teamB");
+        em.persist(teamB);
+
+        Member findMember = em.find(Member.class, member1.getId());
+        findMember.assignTeam(teamB);
+        em.flush();
+        em.clear();
+
+        findMember = em.find(Member.class, member1.getId());
+        assertThat(findMember.getTeam().getName()).isEqualTo("teamB");
+    }
+
+    @Test
+    void deleteRelation() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member member1 = new Member("member1", 20);
+        member1.assignTeam(teamA);
+        em.persist(member1);
+        em.flush();
+        em.clear();
+
+        Member findMember = em.find(Member.class, member1.getId());
+        findMember.assignTeam(null);
+        em.flush();
+        em.clear();
+
+        findMember = em.find(Member.class, member1.getId());
+        assertThat(findMember.getTeam()).isNull();
+    }
+
+    @Test
+    void removeTeam_error() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member member1 = new Member("member1", 20);
+        member1.assignTeam(teamA);
+        em.persist(member1);
+
+        Member findMember = em.find(Member.class, member1.getId());
+        em.remove(findMember.getTeam());
+        em.flush();
+        assertThatThrownBy(() -> em.flush())
+                .isInstanceOf(PersistenceException.class); // ConstraintViolationException
+    }
+
+    @Test
+    void removeTeam_success() {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member member1 = new Member("member1", 20);
+        member1.assignTeam(teamA);
+        em.persist(member1);
+
+        Member findMember = em.find(Member.class, member1.getId());
+        Team removeTeam = findMember.getTeam();
+        findMember.assignTeam(null);
+        em.remove(removeTeam);
+        Assertions.assertDoesNotThrow(() -> em.flush());
     }
 }
