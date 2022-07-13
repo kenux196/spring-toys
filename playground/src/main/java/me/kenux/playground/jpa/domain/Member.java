4e7d95feccf95209c661b2dc3b2bd02c8dabb983
skyun <skyun@a2tec.co.kr>
2022-07-13 23:22:40 +0900

    JPA 성능 최적화 방법 공부 1

--- playground/src/main/java/me/kenux/playground/jpa/domain/Member.java
+++ playground/src/main/java/me/kenux/playground/jpa/domain/Member.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 @Table(name = "member")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@EqualsAndHashCode
+//@EqualsAndHashCode
 public class Member {
 
     @Id
@@ -64,5 +64,23 @@ public class Member {
     public Member(String name, Address address) {
         this.name = name;
         this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Member member = (Member) o;
+
+        if (id != null ? !id.equals(member.id) : member.id != null) return false;
+        return name != null ? name.equals(member.name) : member.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
     }
 }
--- playground/src/test/java/me/kenux/playground/jpa/repository/JpaTest.java
+++ playground/src/test/java/me/kenux/playground/jpa/repository/JpaTest.java
@@ -15,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import static org.assertj.core.api.Assertions.*;
+
 @Slf4j
 @SpringBootTest
 @Import(QuerydslConfig.class)
@@ -46,7 +48,7 @@ class JpaTest {
         // findMember 는 준영속 상태
 
         // 둘은 다른 주소값을 가진 인스턴스다.
-        Assertions.assertThat(member ).isSameAs(findMember);
+        assertThat(member).isSameAs(findMember);
     }
 
     @Test
@@ -66,6 +68,73 @@ class JpaTest {
         // findMember 는 준영속 상태
 
         // 둘은 다른 주소값을 가진 인스턴스다.
-        Assertions.assertThat(member).isNotSameAs(findMember);
+        assertThat(member).isNotSameAs(findMember);
+    }
+
+    @Test
+    @DisplayName("영속성 컨텍스트와 프록시1 - 프록시 조회 후 원본 엔티티 조회 시 동일성 보장")
+    @Transactional
+    void persistenceContextAndProxy1() {
+        Member newMember = new Member("member1");
+        em.persist(newMember);
+        em.flush();
+        em.clear();
+
+        Member refMember = em.getReference(Member.class, newMember.getId());
+        Member findMember = em.find(Member.class, newMember.getId());
+
+        log.info("refMember Type = {}", refMember.getClass());
+        log.info("findMember Type = {}", findMember.getClass());
+
+        assertThat(refMember).isSameAs(findMember);
+    }
+
+    @Test
+    @DisplayName("영속성 컨텍스트와 프록시2 - 원본 엔티티로 조회 후 프록시 조회 시 동일성 보장")
+    @Transactional
+    void persistenceContextAndProxy2() {
+        Member newMember = new Member("member1");
+        em.persist(newMember);
+        em.flush();
+        em.clear();
+
+        Member findMember = em.find(Member.class, newMember.getId());
+        Member refMember = em.getReference(Member.class, newMember.getId());
+
+        log.info("refMember Type = {}", refMember.getClass());
+        log.info("findMember Type = {}", findMember.getClass());
+
+        assertThat(refMember).isSameAs(findMember);
+    }
+
+    @Test
+    @DisplayName("프록시 타입 비교 예제")
+    @Transactional
+    void compareProxyType() {
+        Member newMember = new Member("member1");
+        em.persist(newMember);
+        em.flush();
+        em.clear();
+
+        Member refMember = em.getReference(Member.class, newMember.getId());
+        log.info("refMember Type = {}", refMember.getClass());
+
+        assertThat(Member.class).isNotSameAs(refMember.getClass());
+        assertThat(refMember).isInstanceOf(Member.class);
+    }
+
+    @Test
+    @DisplayName("프록시 동등성 비교")
+    @Transactional
+    void proxyEquals() {
+        Member saveMember = new Member("member1");
+        em.persist(saveMember);
+        em.flush();
+        em.clear();
+
+        Member newMember = new Member("member1");
+        Member refMember = em.getReference(Member.class, saveMember.getId());
+
+        assertThat(newMember.equals(refMember)).isTrue();
     }
 }
