fb0b59e9a2ba5a92bb57e64d0af2077561a058d5
skyun <skyun@a2tec.co.kr>
2022-07-13 18:26:39 +0900

    JPA 엔티티 비교 테스트 예제 수정

--- playground/src/test/java/me/kenux/playground/jpa/repository/JpaTest.java
+++ playground/src/test/java/me/kenux/playground/jpa/repository/JpaTest.java
@@ -1,5 +1,6 @@
 package me.kenux.playground.jpa.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import me.kenux.playground.config.QuerydslConfig;
 import me.kenux.playground.jpa.domain.Member;
 import me.kenux.playground.jpa.service.JpaMemberService;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+@Slf4j
 @SpringBootTest
 @Import(QuerydslConfig.class)
 //@Transactional
@@ -27,19 +29,43 @@ class JpaTest {
     JpaMemberService memberService;
 
     @Test
-    @DisplayName("다른 영속성 컨텍스트의 엔티티 비교 시, 동일성 비교는 실패한다. 동등성 비교를 해야 한다.")
-    void join() {
+    @DisplayName("동일한 영속성 컨텍스트의 엔티티 비교 시, 동일성(==) 비교 성공한다.")
+    @Transactional
+    void compareEntity1() {
         // given
         Member member = new Member("member1");
 
         // when
         Long saveId = memberService.join(member);
+        log.info("======= service join end ======");
 
         // then
+        // Participating in existing transaction => 트랜잭션 전파.
         Member findMember = memberRepository.findOne(saveId);
+        log.info("======= repository find one end ======");
         // findMember 는 준영속 상태
 
         // 둘은 다른 주소값을 가진 인스턴스다.
-        Assertions.assertThat(member == findMember).isFalse();
+        Assertions.assertThat(member ).isSameAs(findMember);
+    }
+
+    @Test
+    @DisplayName("다른 영속성 컨텍스트의 엔티티 비교 시, 동일성(==) 비교는 실패한다. 동등성 비교를 해야 한다.")
+    void compareEntity2() {
+        // given
+        Member member = new Member("member1");
+
+        // 아래 둘은 서로 다른 트랜잭션으로 수행된다.
+        // when
+        Long saveId = memberService.join(member);
+        log.info("======= service join end ======");
+
+        // then
+        Member findMember = memberRepository.findOne(saveId);
+        log.info("======= repository find one end ======");
+        // findMember 는 준영속 상태
+
+        // 둘은 다른 주소값을 가진 인스턴스다.
+        Assertions.assertThat(member).isNotSameAs(findMember);
     }
 }
